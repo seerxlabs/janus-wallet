17395f7e366258ee4f6c03288d3cacfbbd83d3d8
kbdancer <non3gov@gmail.com>
2022-11-23 16:07:19 +0800

    feat: create NFT send page

---
+++ src/pages/SendNFT/Form/index.tsx
@@ -0,0 +1,31 @@
+import classnames from 'classnames';
+import styles from './index.module.scss';
+import pictureNFT from './img/nft.webp'
+
+function SendForm() {
+    return (
+        <div className={classnames(styles['form-box'])}>
+            <div className={styles['form-item']}>
+                <label>NFT</label>
+                <div className={styles['info-box']}>
+                    <img src={pictureNFT} alt=""/>
+                    <p><b>SdPunk #3319</b><span>ID: 0x495f...7b5e</span></p>
+                </div>
+            </div>
+            <div className={styles['form-item']}>
+                <label>To</label>
+                <div className={styles['input-box']}>
+                    <input placeholder='Enter address or ID'/>
+                </div>
+                <div className={styles['fee-box']}>
+                    <b>Network fee</b><span>0.000005 ETH</span>
+                </div>
+            </div>
+            <div className={styles['submit-btn']}>
+                <button className={styles['send']}>Send</button>
+            </div>
+        </div>
+    );
+}
+
+export default SendForm;
--- src/pages/SendNFT/index.tsx
+++ src/pages/SendNFT/index.tsx
@@ -1,15 +1,15 @@
 import classnames from 'classnames';
 import styles from './index.module.scss';
 import BackBar from '../../components/BackBar';
-import Menu from '../../components/Menu';
 import PageTitle from '../../components/PageTitle';
+import Form from './Form';
 
 function SendNFTPage() {
     return (
         <div className={classnames(styles['main-page'])}>
             <BackBar />
             <PageTitle ext={false} sub={false} name={'Send NFT'}/>
-            <Menu />
+            <Form />
         </div>
     );
 }
